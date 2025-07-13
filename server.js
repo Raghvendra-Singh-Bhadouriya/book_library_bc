612a7fcd603f2224ceebbc5e79bb284058cc0825
Raghvendra-Singh-Bhadouriya <raghvendrabhadouriya2003@gmail.com>
2025-07-13 23:09:38 +0530

    update book library backend with cors url

--- server.js
+++ server.js
@@ -11,9 +11,26 @@ const authRouter = require("./Routes/authRoute")
 const bookRouter = require("./Routes/booksRoute")
 const myBookRouter = require("./Routes/myBookRoute")
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://book-library-nine-lake.vercel.app"
+];
+
 server.use(cors({
-    origin: "http://localhost:5173"
+  origin: function(origin, callback) {
+    if(!origin) return callback(null, true); // allow server-to-server or curl requests with no origin
+    if(allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true // if you use cookies or authorization headers
 }));
+
+// server.use(cors({
+//     origin: "http://localhost:5173"
+// }));
 server.use("/", authRouter)
 server.use("/", bookRouter)
 server.use("/", myBookRouter)
